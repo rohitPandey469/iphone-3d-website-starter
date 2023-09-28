4fd1be13b979ca1f2414c796e858a8365d45a98e
rohitPandey469 <rd8614196@gmail.com>
2023-09-28 17:15:22 +0530

    Three.js Project>>>

---
+++ src/components/DisplaySection.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const DisplaySection = ({ triggerPreview }) => {
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+  return (
+    <div className="display-section wrapper">
+      <h2 className="title">New</h2>
+      <p className="text">Brilliant.</p>
+      <span className="description">
+        A display that's up to 2x brighter in the sun.
+      </span>
+      <button onClick={triggerPreview} className="button">
+        Try me!
+      </button>
+      <button onClick={handleScrollToTop} className="back-button">
+        TOP
+      </button>
+    </div>
+  );
+};
+
+export default DisplaySection;
